fd016fa3a11ba7d5eb324146da0e02755cfe6b42
griger <gustavo.rg92@gmail.com>
2017-01-21 21:56:36 +0100

    Evaluation methods and cross operator progress. Progress in #5

--- GAgeneric.py
+++ GAgeneric.py
@@ -4,14 +4,23 @@ from evaluator import score, mutationScore
 
 '''
 Implementation of a generic genetic algorithm
-
-
-def cross(firstParent, secondParent):
-
 '''
+
+def cross(parent1, parent2, i,j):
+    n = len(parent1)
+    
+    child1, child2 = parent1, parent2
+    idx = list(range(j+1,n)) + list(0, i)
+    child1["chomosome"][idx] = set(parent2["chomosome"]) - set(parent1["chomosome"][i:j+1])
+    child2["chomosome"][idx] = set(parent1["chomosome"]) - set(parent2["chomosome"][i:j+1])
+
+    child1["score"], child2["score"] = score(child1["chomosome"]), score(child2["chomosome"])
+
+    return child1, child2
+
 def mutate(individual, i, j):
-    individual["score"] = mutationScore(individual, i, j)
     individual["chromosome"][i], individual["chromosome"][j] = individual["chromosome"][j], individual["chromosome"][i]
+    individual["score"] = mutationScore(individual["chromosome", i, j)
 
 
 def AG (parameters, problemDim, distanceMtx, weightMtx):
--- evaluator.py
+++ evaluator.py
@@ -1,2 +1,54 @@
-def score(distancemMtx, weightMtx, chromosome):
-    return 42.0
+import numpy as np
+
+class Evaluator:
+    def __init__(self, problemDim, weightMtx, distanceMtx):
+        self.dMtx = distanceMtx
+        self.wMtx = weightMtx
+        self.dim = problemDim
+
+    def checkScore(self, chromosome, score):
+        actualScore = 0.0
+        
+        for i in range(self.dim):
+            for j in range(self.dim):
+                actualScore += self.wMtx[i][j] * self.dMtx[chromosome[i]][chromosome[j]]
+
+        if (actualScore == score):
+            print("The score is correct.")
+        else:
+            print(f"{score} is not correct for {chromosome}. The actual score is {actualScore}.")
+
+    def mutationScore(self, currentChromosome, newChromosome, currentScore, i, j): # i <= j TODO
+        #newChromosome = currentChromosome
+        #newChromosome[i], newChromosome[j] = newChromosome[j], newChromosome[i]
+        print(f"newChromosome: {newChromosome}")
+        
+        mutationScore = currentScore - sum(np.sum(self.wMtx[[i,j], :] * self.dMtx[currentChromosome[[i,j], None], currentChromosome], 1))
+        mutationScore += sum(np.sum(self.wMtx[[i,j], :] * self.dMtx[newChromosome[[i,j], None], newChromosome], 1))
+
+        idx = list(range(self.dim)) ; del(idx[i]) ; del(idx[j-1])
+        
+        mutationScore -= sum(sum(self.wMtx[idx, [i,j]] * self.dMtx[currentChromosome[idx, None], currentChromosome[[i,j]]]))
+        mutationScore += sum(sum(self.wMtx[idx, [i,j]] * self.dMtx[newChromosome[idx, None], newChromosome[[i,j]]]))
+
+        return mutationScore
+
+    def mutationScore2(self, currentChromosome, currentScore, i, j): # i <= j TODO
+        newChromosome = np.copy(currentChromosome)
+        newChromosome[i], newChromosome[j] = newChromosome[j], newChromosome[i]
+        print(f"newChromosome: {newChromosome}")
+        print(f"currentChromosome: {currentChromosome}")
+        
+        mutationScore = currentScore - sum(np.sum(self.wMtx[[i,j], :] * self.dMtx[currentChromosome[[i,j], None], currentChromosome], 1))
+        mutationScore += sum(np.sum(self.wMtx[[i,j], :] * self.dMtx[newChromosome[[i,j], None], newChromosome], 1))
+
+        idx = list(range(self.dim)) ; del(idx[i]) ; del(idx[j-1])
+        
+        mutationScore -= sum(sum(self.wMtx[idx, [i,j]] * self.dMtx[currentChromosome[idx, None], currentChromosome[[i,j]]]))
+        mutationScore += sum(sum(self.wMtx[idx, [i,j]] * self.dMtx[newChromosome[idx, None], newChromosome[[i,j]]]))
+
+        return mutationScore
+
+
+    def score(self, chromosome):
+        return sum(np.sum(self.wMtx * self.dMtx[chromosome[:, None], chromosome], 1))
